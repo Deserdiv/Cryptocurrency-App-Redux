856eb8f91320ffba8e4c53afe53ef4995184faec
Deserdiv <116967915+Deserdiv@users.noreply.github.com>
2023-02-01 00:43:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
         <div className='footer'>
         <Typography.Title  level={5} style={{ color: 'white', textAlign: 'center'}}>
           CryptoWorld <br />
-          Created by Damilola Adegbemile </Typography.Title>
+           Deserdiv </Typography.Title>
           <Space>
             <Link to='/'>Home</Link>
             <a href ='https://deserdiv.netlify.app/' target='blank'>MyCrypto</a>
